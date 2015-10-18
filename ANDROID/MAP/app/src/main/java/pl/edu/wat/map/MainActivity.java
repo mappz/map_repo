41ee1155d2b2693e9b0c977790574a65c5232e98
zarcel@gmail.com <zarcel@gmail.com>
2015-10-18 12:29:58 +0200

    [#9707] Android - logowanie anonimowe i email/haslo

--- ANDROID/MAP/app/src/main/java/pl/edu/wat/map/MainActivity.java
+++ ANDROID/MAP/app/src/main/java/pl/edu/wat/map/MainActivity.java
@@ -1,38 +1,292 @@
 package pl.edu.wat.map;
 
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.Map;
+
+public class MainActivity extends ActionBarActivity  {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    /* *************************************
+     *              GENERAL                *
+     ***************************************/
+    /* TextView that is used to display information about the logged in user */
+    private TextView mLoggedInStatusTextView;
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+
+
+    /* A dialog that is presented until the Firebase authentication finished. */
+    private ProgressDialog mAuthProgressDialog;
+
+    /* A reference to the Firebase */
+    private Firebase mFirebaseRef;
+
+    /* Data from the authenticated user */
+    private AuthData mAuthData;
+
+    /* Listener for Firebase session changes */
+    private Firebase.AuthStateListener mAuthStateListener;
+
+
+
+
+    /* *************************************
+     *              PASSWORD               *
+     ***************************************/
+    private Button mPasswordLoginButton;
+
+    /* *************************************
+     *            ANONYMOUSLY              *
+     ***************************************/
+    private Button mAnonymousLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(getApplicationContext());
+        /* Load the view and display it */
         setContentView(R.layout.activity_main);
+
+
+        mEmailEditText = (EditText) findViewById(R.id.email);
+        mPasswordEditText = (EditText) findViewById(R.id.password);
+
+
+        /* *************************************
+         *               PASSWORD              *
+         ***************************************/
+        mPasswordLoginButton = (Button) findViewById(R.id.login_with_password);
+        mPasswordLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginWithPassword();
+            }
+        });
+
+        /* *************************************
+         *              ANONYMOUSLY            *
+         ***************************************/
+        /* Load and setup the anonymous login button */
+        mAnonymousLoginButton = (Button) findViewById(R.id.login_anonymously);
+        mAnonymousLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginAnonymously();
+            }
+        });
+
+        /* *************************************
+         *               GENERAL               *
+         ***************************************/
+        mLoggedInStatusTextView = (TextView) findViewById(R.id.login_status);
+
+        /* Create the Firebase ref that is used for all authentication with Firebase */
+        mFirebaseRef = new Firebase(getResources().getString(R.string.firebase_url));
+
+        /* Setup the progress dialog that is displayed later when authenticating with Firebase */
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+        mAuthProgressDialog.show();
+
+        mAuthStateListener = new Firebase.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(AuthData authData) {
+                mAuthProgressDialog.hide();
+                setAuthenticatedUser(authData);
+            }
+        };
+        /* Check if the user is authenticated with Firebase already. If this is the case we can set the authenticated
+         * user and hide hide any login buttons */
+        mFirebaseRef.addAuthStateListener(mAuthStateListener);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+        // if changing configurations, stop tracking firebase session.
+        mFirebaseRef.removeAuthStateListener(mAuthStateListener);
+    }
+
+    /**
+     * This method fires when any startActivityForResult finishes. The requestCode maps to
+     * the value passed into startActivityForResult.
+     */
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        /* If a user is currently authenticated, display a logout menu */
+        if (this.mAuthData != null) {
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
+            logout();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Unauthenticate from Firebase and from providers where necessary.
+     */
+    private void logout() {
+        if (this.mAuthData != null) {
+            /* logout of Firebase */
+            mFirebaseRef.unauth();
+            /* Update authenticated user and show login buttons */
+            setAuthenticatedUser(null);
+        }
+    }
+
+    /**
+     * This method will attempt to authenticate a user to firebase given an oauth_token (and other
+     * necessary parameters depending on the provider)
+     */
+    private void authWithFirebase(final String provider, Map<String, String> options) {
+        if (options.containsKey("error")) {
+            showErrorDialog(options.get("error"));
+        } else {
+            mAuthProgressDialog.show();
+            if (provider.equals("twitter")) {
+                // if the provider is twitter, we pust pass in additional options, so use the options endpoint
+                mFirebaseRef.authWithOAuthToken(provider, options, new AuthResultHandler(provider));
+            } else {
+                // if the provider is not twitter, we just need to pass in the oauth_token
+                mFirebaseRef.authWithOAuthToken(provider, options.get("oauth_token"), new AuthResultHandler(provider));
+            }
+        }
+    }
+
+    /**
+     * Once a user is logged in, take the mAuthData provided from Firebase and "use" it.
+     */
+    private void setAuthenticatedUser(AuthData authData) {
+        if (authData != null) {
+            /* Hide all the login buttons */
+
+
+            mPasswordLoginButton.setVisibility(View.GONE);
+            mAnonymousLoginButton.setVisibility(View.GONE);
+            mEmailEditText.setVisibility(View.GONE);
+            mPasswordEditText.setVisibility(View.GONE);
+            mLoggedInStatusTextView.setVisibility(View.VISIBLE);
+            /* show a provider specific status text */
+            String name = null;
+            if (authData.getProvider().equals("anonymous")
+                    || authData.getProvider().equals("password")) {
+                name = authData.getUid();
+            } else {
+                Log.e(TAG, "Invalid provider: " + authData.getProvider());
+            }
+            if (name != null) {
+                mLoggedInStatusTextView.setText("Logged in as " + name + " (" + authData.getProvider() + ")");
+            }
+        } else {
+            /* No authenticated user show all the login buttons */
+
+            mPasswordLoginButton.setVisibility(View.VISIBLE);
+            mAnonymousLoginButton.setVisibility(View.VISIBLE);
+            mEmailEditText.setVisibility(View.VISIBLE);
+            mPasswordEditText.setVisibility(View.VISIBLE);
+            mLoggedInStatusTextView.setVisibility(View.GONE);
+        }
+        this.mAuthData = authData;
+        /* invalidate options menu to hide/show the logout button */
+        supportInvalidateOptionsMenu();
+    }
+
+    /**
+     * Show errors to users
+     */
+    private void showErrorDialog(String message) {
+        new AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    /**
+     * Utility class for authentication results
+     */
+    private class AuthResultHandler implements Firebase.AuthResultHandler {
+
+        private final String provider;
+
+        public AuthResultHandler(String provider) {
+            this.provider = provider;
+        }
+
+        @Override
+        public void onAuthenticated(AuthData authData) {
+            mAuthProgressDialog.hide();
+            Log.i(TAG, provider + " auth successful");
+            setAuthenticatedUser(authData);
+        }
+
+        @Override
+        public void onAuthenticationError(FirebaseError firebaseError) {
+            mAuthProgressDialog.hide();
+            showErrorDialog(firebaseError.toString());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    /* ************************************
+     *              PASSWORD              *
+     **************************************
+     */
+    public void loginWithPassword() {
+        mAuthProgressDialog.show();
+        mFirebaseRef.authWithPassword(mEmailEditText.getText().toString(), mPasswordEditText.getText().toString(), new AuthResultHandler("password"));
+    }
+
+    /* ************************************
+     *             ANONYMOUSLY            *
+     **************************************
+     */
+    private void loginAnonymously() {
+        mAuthProgressDialog.show();
+        mFirebaseRef.authAnonymously(new AuthResultHandler("anonymous"));
     }
 }
