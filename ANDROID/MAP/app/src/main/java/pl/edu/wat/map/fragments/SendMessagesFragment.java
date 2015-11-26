71495617b60efa6006c5338c57a13b8b285c2ec4
zarcel@gmail.com <zarcel@gmail.com>
2015-11-26 15:21:24 +0100

    [#10134]  Dodanie widoku wysyłania wiadomosci

--- ANDROID/MAP/app/src/main/java/pl/edu/wat/map/fragments/SendMessagesFragment.java
+++ ANDROID/MAP/app/src/main/java/pl/edu/wat/map/fragments/SendMessagesFragment.java
@@ -1,12 +1,16 @@
 package pl.edu.wat.map.fragments;
 
 import android.app.Activity;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import pl.edu.wat.map.R;
 
@@ -18,8 +22,10 @@ import pl.edu.wat.map.R;
  * Use the {@link SendMessagesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SendMessagesFragment extends Fragment {
+public class SendMessagesFragment extends Fragment implements View.OnClickListener {
 
+    private EditText messageEditText;
+    private Button sendButton;
     public static SendMessagesFragment newInstance(String param1, String param2) {
         SendMessagesFragment fragment = new SendMessagesFragment();
         return fragment;
@@ -37,8 +43,12 @@ public class SendMessagesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_send_messages, container, false);
+        View v = inflater.inflate(R.layout.fragment_send_messages, container, false);
+        messageEditText = (EditText) v.findViewById(R.id.message_text_send);
+        sendButton = (Button) v.findViewById(R.id.button_send);
+        sendButton.setOnClickListener(this);
+
+        return v ;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -53,6 +63,16 @@ public class SendMessagesFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Context context = getActivity();
+        CharSequence text = "Wyslano: " + messageEditText.getText();
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
 
